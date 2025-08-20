84708ef07ef20412621e24611c15f4837a1b96ff
Alec Faustino <alecfaustino@outlook.com>
2025-08-20 16:47:37 -0600

    add base navbar styles and html

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+export default function Navbar() {
+  return (
+    <div className="px-6 py-3 bg-gray-800 flex flex-row border-b border-b-grey-500">
+      <div className="flex flex-row flex-1 gap-4">
+        <h1>Logo img</h1>
+        <input className="border rounded-3xl p-2 h-7"placeholder="search"></input>
+      </div>
+
+      <div className="flex flex-row flex-3 gap-4 justify-end">
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+        <h1> | </h1>
+        <h1>Temp</h1>
+        <h1>Temp</h1>
+      </div>
+    </div>
+  )
+}
