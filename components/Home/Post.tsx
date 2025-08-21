ac034560e78bad660a0ef5fd465206f7a26ce928
Alec Faustino <alecfaustino@outlook.com>
2025-08-20 21:34:59 -0600

    removed redundant tailwind classes

--- components/Home/Post.tsx
+++ components/Home/Post.tsx
@@ -6,36 +6,41 @@ import { faImage, faVideo, faPenToSquare} from "@fortawesome/free-solid-svg-icon
 export default function Post() {
   return (
     // Main div for the border bottom with the line separating the components in the middle column
-    <div className="border-b border-b-grey-500 m-6 h-auto items-center">
-    {/* the post section */}
-     <div className="m-6 h-auto bg-gray-800 flex flex-col rounded-md content-center">
-      {/* the photo and the input field */}
-      <div className="flex flex-1 p-4">
-        <Image
-            src="/globe.svg"   
+    <div className="border-b border-gray-500 m-6">
+
+      {/* the post section */}
+      <div className="m-6 bg-gray-800 flex flex-col rounded-md">
+
+        {/* the photo and the input field */}
+        <div className="flex p-4 gap-4 items-center">
+          <Image
+            src="/globe.svg"
             alt="Profile photo"
             width={80}
             height={80}
-            className="px-4 "
-            />
-        <input placeholder="Start a Post" className="px-4 h-12 flex flex-[2] focus:outline-none border rounded-4xl"/>
+            className=""
+          />
+          <input
+            placeholder="Start a Post"
+            className="flex-1 h-12 px-4 border rounded-full focus:outline-none"
+          />
         </div>
 
-      {/* Video, Photo, Article */}
-      <div className="flex justify-around p-4" >
-        <div className="flex gap-2">
-         <FontAwesomeIcon icon={faVideo} size="xl" className="text-green-400"/>
-         <span>Video</span>
+        {/* Video, Photo, Article */}
+        <div className="flex justify-around p-4">
+          <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-700 px-2 py-1 rounded-md">
+            <FontAwesomeIcon icon={faVideo} size="lg" className="text-green-400"/>
+            <span>Video</span>
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-700 px-2 py-1 rounded-md">
+            <FontAwesomeIcon icon={faImage} size="lg" className="text-blue-500"/>
+            <span>Photo</span>
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-700 px-2 py-1 rounded-md">
+            <FontAwesomeIcon icon={faPenToSquare} size="lg" className="text-orange-300"/>
+            <span>Write article</span>
+          </div>
         </div>
-        <div className="flex gap-2">
-         <FontAwesomeIcon icon={faImage} size="xl" className="text-blue-500"/>
-         <span>Photo</span>
-        </div>
-        <div className="flex gap-2">
-          <FontAwesomeIcon icon={faPenToSquare} size="xl" className="text-orange-300"/>
-          <span>Write article</span>
-        </div>
-      </div>
       </div>
     </div>
   )
