acc12dcedb6bb2cfad7d9545128105e328da0e1f
Alec Faustino <alecfaustino@outlook.com>
2025-08-20 19:38:14 -0600

    left side icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,32 +6,33 @@ export default function Navbar() {
   return (
     <nav className="px-6 py-3 bg-gray-800 flex flex-row border-b border-b-grey-500">
       {/* The logo and search bar div */}
-      <div className="flex flex-row flex-1 gap-4">
+      <div className="flex flex-row flex-1 gap-4 items-center">
         <FontAwesomeIcon icon={faLinkedin} size="2xl" />
 
 
         {/* the search bar div to group icon and input */}
-        <div className="flex flex-row border rounded-2xl">
-        <FontAwesomeIcon icon={faSearch} className="p-2"/>
+        <div className="flex flex-row border rounded-4xl">
+        <FontAwesomeIcon icon={faSearch} className="p-4"/>
         <input >
         
         </input>
         </div>
       </div>
 
-      <div className="flex flex-row flex-3 gap-4 justify-end">
-        <h1>Temp</h1>
-        <h1>Temp</h1>
-        <h1>Temp</h1>
-        <h1>Temp</h1>
-        <h1>Temp</h1>
-        <h1>Temp</h1>
-        <h1>Temp</h1>
-        <h1>Temp</h1>
-        <h1>Temp</h1>
-        <h1> | </h1>
-        <h1>Temp</h1>
-        <h1>Temp</h1>
+      <div className="flex items-center flex-3 gap-4 justify-end">
+        <div className="flex flex-col items-center">
+          <FontAwesomeIcon icon={faHome} size="lg"/>
+          <span>Home</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <FontAwesomeIcon icon={faBell} size="lg" />
+          <span>Notifications</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <FontAwesomeIcon icon={faUser} size="lg" />
+          <span>Profile</span>
+        </div>
+        
       </div>
     </nav>
   )
