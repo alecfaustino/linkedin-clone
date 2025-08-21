17f84d7dad4f9aa7091b4ec00bff70351f73729a
Alec Faustino <alecfaustino@outlook.com>
2025-08-20 19:28:01 -0600

    left side navbar styling with linkedin icon and searchbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faSearch, faBell, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 export default function Navbar() {
   return (
-    <div className="px-6 py-3 bg-gray-800 flex flex-row border-b border-b-grey-500">
+    <nav className="px-6 py-3 bg-gray-800 flex flex-row border-b border-b-grey-500">
+      {/* The logo and search bar div */}
       <div className="flex flex-row flex-1 gap-4">
-        <h1>Logo img</h1>
-        <input className="border rounded-3xl p-2 h-7"placeholder="search"></input>
+        <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+
+
+        {/* the search bar div to group icon and input */}
+        <div className="flex flex-row border rounded-2xl">
+        <FontAwesomeIcon icon={faSearch} className="p-2"/>
+        <input >
+        
+        </input>
+        </div>
       </div>
 
       <div className="flex flex-row flex-3 gap-4 justify-end">
@@ -20,6 +33,6 @@ export default function Navbar() {
         <h1>Temp</h1>
         <h1>Temp</h1>
       </div>
-    </div>
+    </nav>
   )
 }
